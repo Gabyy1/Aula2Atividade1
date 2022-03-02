22b7df03e395022d4055ff3e4944c91888e9ce9e
Gabrizinha <gabiemanu18@gmail.com>
2022-03-02 16:07:15 -0300

    Primeiro commit

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -1,13 +1,68 @@
 package controller;
 
-public class RedesController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-	 public RedesController() {
-		 super();
-	 }
-	 
-	 public String os() {
+
+public class RedesController {
+	public RedesController() {
+		super();
+
+	  }
+	
+	public String os() {
 		String os = System.getProperty("os.name");
 		return os;
-	 }
-}
+	}
+	
+	
+    public void readProcess(String ip) {
+    	try {
+    		Process p = Runtime.getRuntime().exec(ip);
+    		InputStream fluxo = p.getInputStream();
+    		InputStreamReader leitor = new InputStreamReader(fluxo);
+    		BufferedReader buffer = new BufferedReader(leitor);
+    		String linha = buffer.readLine();
+    		while (linha != null) {
+    			System.out.println(linha);
+    			linha = buffer.readLine();
+    		}
+    		buffer.close();
+    		leitor.close();
+    		fluxo.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	}
+    
+    public void readTraceRoute(String ping) {
+    	try {
+    		Process p = Runtime.getRuntime().exec(ping);
+    		InputStream fluxo = p.getInputStream();
+    		InputStreamReader leitor = new InputStreamReader(fluxo);
+    		BufferedReader buffer = new BufferedReader(leitor);
+    		String linha = buffer.readLine();
+    		while (linha != null) {
+    			System.out.println(linha);
+    			linha = buffer.readLine();
+    		}
+    		buffer.close();
+    		leitor.close();
+    		fluxo.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	}
+    
+  
+	
+	}
+		
+	
+	
+
+
+
+
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,16 +1,47 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.RedesController;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		
-		RedesController redesCont = new RedesController();
-		String os = redesCont.os();
-		
-		System.out.println(os);
+	 public static void main (String []args) {
+		 RedesController redesCont = new RedesController();
+		 
+		 int option = 0 ;
+				 do {
+					 option = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Mostrar Adaptadores de rede com IPv4\n" +
+				                                                                 "2 - Mostrar média de ping\n " +
+							                                                     "3 - Sair"));
+				 
+		 switch(option) {
+		 case 1:
 
-	}
-
+			    String ip = "ipconfig";
+		        redesCont.readProcess(ip);
+		        break;
+		        
+		 case 2 :
+			 
+			  String ping = "PING -4 -n 10 www.google.com.br";
+	          redesCont.readTraceRoute(ping);
+			  break;
+			  
+		 case 3 :
+			 JOptionPane.showInputDialog(null, "Encerrando"); 
+			 
+	     break;
+	     
+		 }
+		 
+	 } while (option != 3);
+		 
+			
+	
+	 }
 }
+
+
+
+
