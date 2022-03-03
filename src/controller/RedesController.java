b2dd642beb25240e3da02a35cd17b86992d4370c
Gabrizinha <gabiemanu18@gmail.com>
2022-03-03 20:04:14 -0300

    commit

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -19,6 +19,18 @@ public class RedesController {
 	
 	
     public void readProcess(String ip) {
+    	String os = System.getProperty("os.name");
+    	String IP = "";
+    	String IF = "";
+    	
+    	if (os.contains("Windows")) {
+    		IP = "ipconfig" + ip;
+    		}
+    	
+    	if (os.contains("Linux")) {
+    	    IF = "ifconfig" + ip;
+    	}
+    	
     	try {
     		Process p = Runtime.getRuntime().exec(ip);
     		InputStream fluxo = p.getInputStream();
@@ -38,6 +50,17 @@ public class RedesController {
     	}
     
     public void readTraceRoute(String ping) {
+    	String os = System.getProperty("os.name");
+    	String pingw = "";
+    	String pingl = "";
+    	
+    	if (os.contains("Windows")) {
+    		pingw = "PING -4 -n 10";
+    		}
+    	
+    	if (os.contains("Linux")) {
+    	    pingl = "ping -4 -c 10";
+    	}
     	try {
     		Process p = Runtime.getRuntime().exec(ping);
     		InputStream fluxo = p.getInputStream();
